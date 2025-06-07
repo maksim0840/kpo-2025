b3c841d73288679fad76e4d70b6493b2f79c75a3
maksim0840 <maksim0840@gmail.com>
2025-06-07 11:55:12 +0300

    Add SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
 
